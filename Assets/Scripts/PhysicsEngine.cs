abdf23fbfd9ea8f017ac3d0bb6c1debb73af9dee
Alice Robinson <valkrysa@gmail.com>
2017-09-13 23:37:35 -0700

    rendering of representation of physics forces acting on object

---
+++ Assets/Scripts/PhysicsEngine.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsEngine : MonoBehaviour {
+
+    public List<Vector3> forceVectorList;
+    public Vector3 velocityVector; // average velocity in fixed update
+    public Vector3 netVelocityVector;
+
+    // Use this for initialization
+    void Start () {
+        AddForces();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate () {
+        AddForces();
+        transform.position += velocityVector * Time.deltaTime;
+    }
+
+    private void AddForces () {
+        Vector3 netForces = new Vector3(0, 0, 0);
+
+        foreach(Vector3 forceVector in forceVectorList) {
+            netForces += forceVector;
+        }
+
+        netVelocityVector = netForces;
+    }
+}
